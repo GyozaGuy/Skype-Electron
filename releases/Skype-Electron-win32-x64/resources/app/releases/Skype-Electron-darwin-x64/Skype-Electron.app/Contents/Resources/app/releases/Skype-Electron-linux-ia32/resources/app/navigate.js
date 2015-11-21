c6ca9cf33ca31cedeee10e3900603e4b9571b48f
Steven Stromberg <gyozaguy@gmail.com>
2015-11-21 13:27:53 -0700

    Removed release folders, replaced with archives

--- releases/Skype-Electron-win32-x64/resources/app/releases/Skype-Electron-darwin-x64/Skype-Electron.app/Contents/Resources/app/releases/Skype-Electron-linux-ia32/resources/app/navigate.js
+++
@@ -1,5 +0,0 @@
-var webview = document.getElementById('webview');
-webview.addEventListener('new-window', function(e) {
-  e.preventDefault();
-  require('shell').openExternal(e.url);
-});
--- releases/Skype-Electron-win32-x64/resources/app/releases/Skype-Electron-linux-x64/resources/app/releases/Skype-Electron-darwin-x64/Skype-Electron.app/Contents/Resources/app/releases/Skype-Electron-linux-ia32/resources/app/releases/Skype-Electron-linux-x64/resources/app/README.md
+++
@@ -1,2 +0,0 @@
-# Skype-Electron
-An Electron Skype app designed for use on Linux systems.
