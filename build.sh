16c7713f2caa582a4e6058f6de0a2c6059adfb1a
Steven Stromberg <steven.stromberg@intel.com>
2016-06-08 11:13:52 -0600

    Updated build version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Skype-Electron
-VERSION=1.0.1
+VERSION=1.2.2
 electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
